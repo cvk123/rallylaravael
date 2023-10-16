f4bd15c5833a8fd2e6f06b12d9ce39dbb4f0a132
Pavel Vintr <pavelvintr11@seznam.cz>
2023-10-17 00:56:51 +0200

    Update Team.php

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     use HasFactory;
+    public $timestamps = false;
 
     /**
      * Define a many-to-many relationship for racers.
